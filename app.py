d45116f20feca7eec2718777d438131decf9eb93
Esther okpara <shinystar.oe@gmail.com>
2022-11-17 14:25:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ with tab1:
 
 
         def database():
-            conn = sqlite3.connect('mydata1.db')
+            conn = sqlite3.connect('data.db')
             c = conn.cursor()
             c.execute("""CREATE TABLE IF NOT EXISTS users_symptom(symptoms TEXT,recommendations TEXT)""")
             c.execute("INSERT INTO users_symptom VALUES(?,?)",(text,result))
